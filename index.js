713e6455647440e2f422bdc11b6e028f9ef2800c
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-07-07 16:12:35 -0300

    exercise-tracker

--- index.js
+++ index.js
@@ -91,14 +91,38 @@ app.get("/api/users/:_id/logs", async (req, res) => {
   const { from, to, limit } = req.query;
   const id = req.params._id;
   const user = await User.findById(id);
-  if(!user){
+  if (!user) {
     res.send('no se pudo encontrar el usuario');
     return;
   }
   let dateObj = {}
   if (from) {
-    
+    dateObj["$mayorIgualA"] = new Date(from)
   }
+  if (to) {
+    dateObj("$menorIgualA") = new Date(to)
+  }
+  let filter = {
+    user_id: id
+  }
+  if (from || to) {
+    filter.date = dateObj;
+  }
+
+  const exercises = await Exercise.find(filter).limit(+limit ?? 500)
+
+  const log = exercises.map(e => ({
+    description: e.description,
+    duration: e.duration,
+    date: e.date.toDateString()
+  }))
+
+  res.json({
+    username: user.username,
+    count: exercises.length,
+    _id: user._id,
+    log
+  })
 })
 
 const listener = app.listen(process.env.PORT || 3030, () => {
